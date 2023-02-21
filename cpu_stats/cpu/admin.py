99b48f0c718210336028e8050793a4ad6b46fad9
Натальчук Александр <ss2000nat@yandex.ru>
2023-02-21 09:09:46 +0300

    Add psutil in req.txt

--- cpu_stats/cpu/admin.py
+++ cpu_stats/cpu/admin.py
@@ -5,7 +5,7 @@ from .models import Stats
 
 
 class StatsAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'cpu_load', 'avg_load', 'graph',)
+    list_display = ('pk', 'cpu_load', 'avg_load',)
     search_fields = ('cpu_load',)
     list_editable = ('cpu_load', 'avg_load',)
     empty_value_display = '-пусто-'
--- cpu_stats/cpu/models.py
+++ cpu_stats/cpu/models.py
@@ -5,11 +5,6 @@ from django.db import models
 class Stats(models.Model):
     cpu_load = models.FloatField()
     avg_load = models.FloatField()
-    graph = models.ImageField(
-        'График',
-        upload_to='graphs/%Y/%m/%d/%H/%M/',
-        blank=True
-    )
     measure_time = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
